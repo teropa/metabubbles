69bbeebfad6707d3582cd65ce33ab625bddcc7e8
Tero Parviainen <tero@teropa.info>
2016-02-28 11:02:44 +0200

    load polyfills/zone.js through webpack

---
+++ app/polyfills.ts
@@ -0,0 +1,4 @@
+import 'es6-shim';
+import 'es6-promise';
+import 'reflect-metadata';
+import 'zone.js/dist/zone-microtask';
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './app/main.js',
+  entry: ['./app/polyfills.js', './app/main.js'],
   output: {
     path: __dirname,
     filename: 'bundle.js'
